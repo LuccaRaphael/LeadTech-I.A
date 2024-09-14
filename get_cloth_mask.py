b23d2aefcb95b96311cb9e2fd6e3ed66d7d41532
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-13 21:58:30 -0300

    Fix Code

--- get_cloth_mask.py
+++ get_cloth_mask.py
@@ -21,31 +21,24 @@ x = transform(image=padded_image)["image"]
 x = torch.unsqueeze(tensor_from_rgb_image(x), 0)
 
 with torch.no_grad():
-    prediction = model(x)[0][0]
+  prediction = model(x)[0][0]
 
 mask = (prediction > 0).cpu().numpy().astype(np.uint8)
 mask = unpad(mask, pads)
 
-img = np.full((1024, 768, 3), 255)
-seg_img = np.full((1024, 768), 0)
+img=np.full((1024,768,3), 255)
+seg_img=np.full((1024,768), 0)
 
-b = cv2.imread("./static/cloth_web.jpg")
-b_img = mask * 255
+b=cv2.imread("./static/cloth_web.jpg")
+b_img = mask* 255
+                 
+if b.shape[1]<=600 and b.shape[0]<=500:
+    b=cv2.resize(b,(int(b.shape[1]*1.2),int(b.shape[0]*1.2)))
+    b_img=cv2.resize(b_img,(int(b_img.shape[1]*1.2),int(b_img.shape[0]*1.2)))
+shape=b_img.shape
+img[int((1024-shape[0])/2): 1024-int((1024-shape[0])/2),int((768-shape[1])/2):768-int((768-shape[1])/2)]=b
+seg_img[int((1024-shape[0])/2): 1024-int((1024-shape[0])/2),int((768-shape[1])/2):768-int((768-shape[1])/2)]=b_img
 
-# Calculate the exact dimensions that will fit into the img array
-# Ensure both dimensions are even to avoid mismatch
-exact_height = 1024 - (1024 % 2)
-exact_width = 768 - (768 % 2)
 
-# Resize b and b_img to these exact dimensions
-b = cv2.resize(b, (exact_width, exact_height))
-b_img = cv2.resize(b_img, (exact_width, exact_height))
-
-# Now, the dimensions of b and b_img should match the slice of img
-# You can proceed with the assignment without encountering the ValueError
-img[int((1024-exact_height)/2): 1024-int((1024-exact_height)/2), int((768-exact_width)/2):768-int((768-exact_width)/2)] = b
-seg_img[int((1024-exact_height)/2): 1024-int((1024-exact_height)/2), int((768-exact_width)/2):768-int((768-exact_width)/2)] = b_img
-
-# Save the images
-cv2.imwrite("./HR-VITON-main/test/test/cloth/00001_00.jpg", img)
-cv2.imwrite("./HR-VITON-main/test/test/cloth-mask/00001_00.jpg", seg_img)
+cv2.imwrite("./HR-VITON-main/test/test/cloth/00001_00.jpg",img)
+cv2.imwrite("./HR-VITON-main/test/test/cloth-mask/00001_00.jpg",seg_img)
--- main.py
+++ main.py
@@ -1,51 +1,91 @@
-from pylab import imshow
-import numpy as np
+import os, sys
 import cv2
-import torch
-import albumentations as albu
-from iglovikov_helper_functions.utils.image_utils import load_rgb, pad, unpad
-from iglovikov_helper_functions.dl.pytorch.utils import tensor_from_rgb_image
-from cloths_segmentation.pre_trained_models import create_model
+from PIL import Image
+import numpy as np
+import glob
 import warnings
-warnings.filterwarnings("ignore")
+import argparse
+from cloths_segmentation.pre_trained_models import create_model
 
-model = create_model("Unet_2020-10-30")
-model.eval()
-image = load_rgb("./static/cloth_web.jpg")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--background', type=bool, default=True, help='Define removing background or not')
+    opt = parser.parse_args()
+    
+    # Read input image
+    img=cv2.imread("./static/origin_web.jpg")
+    ori_img=cv2.resize(img,(768,1024))
+    cv2.imwrite("./origin.jpg",ori_img)
 
-transform = albu.Compose([albu.Normalize(p=1)], p=1)
+    # Resize input image
+    img=cv2.imread('origin.jpg')
+    img=cv2.resize(img,(384,512))
+    cv2.imwrite('resized_img.jpg',img)
 
-padded_image, pads = pad(image, factor=32, border=cv2.BORDER_CONSTANT)
+    # Get mask of cloth
+    print("Get mask of cloth\n")
+    terminnal_command = "python get_cloth_mask.py" 
+    os.system(terminnal_command)
 
-x = transform(image=padded_image)["image"]
-x = torch.unsqueeze(tensor_from_rgb_image(x), 0)
+    # Get openpose coordinate using posenet
+    print("Get openpose coordinate using posenet\n")
+    terminnal_command = "python posenet.py" 
+    os.system(terminnal_command)
 
-with torch.no_grad():
-    prediction = model(x)[0][0]
+    # Generate semantic segmentation using Graphonomy-Master library
+    print("Generate semantic segmentation using Graphonomy-Master library\n")
+    os.chdir("./Graphonomy-master")
+    terminnal_command ="python exp/inference/inference.py --loadmodel ./inference.pth --img_path ../resized_img.jpg --output_path ../ --output_name /resized_segmentation_img"
+    os.system(terminnal_command)
+    os.chdir("../")
 
-mask = (prediction > 0).cpu().numpy().astype(np.uint8)
-mask = unpad(mask, pads)
+    # Remove background image using semantic segmentation mask
+    mask_img=cv2.imread('./resized_segmentation_img.png',cv2.IMREAD_GRAYSCALE)
+    mask_img=cv2.resize(mask_img,(768,1024))
+    k = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    mask_img = cv2.erode(mask_img, k)
+    img_seg=cv2.bitwise_and(ori_img,ori_img,mask=mask_img)
+    back_ground=ori_img-img_seg
+    img_seg=np.where(img_seg==0,215,img_seg)
+    cv2.imwrite("./seg_img.png",img_seg)
+    img=cv2.resize(img_seg,(768,1024))
+    cv2.imwrite('./HR-VITON-main/test/test/image/00001_00.jpg',img)
 
-img = np.full((1024, 768, 3), 255)
-seg_img = np.full((1024, 768), 0)
+    # Generate grayscale semantic segmentation image
+    terminnal_command ="python get_seg_grayscale.py"
+    os.system(terminnal_command)
 
-b = cv2.imread("./static/cloth_web.jpg")
-b_img = mask * 255
+    # Generate Densepose image using detectron2 library
+    print("\nGenerate Densepose image using detectron2 library\n")
+    terminnal_command ="python detectron2/projects/DensePose/apply_net.py dump detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml \
+    https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl \
+    origin.jpg --output output.pkl -v"
+    os.system(terminnal_command)
+    terminnal_command ="python get_densepose.py"
+    os.system(terminnal_command)
 
-# Calculate the exact dimensions that will fit into the img array
-# Ensure both dimensions are even to avoid mismatch
-exact_height = 1024 - (1024 % 2)
-exact_width = 768 - (768 % 2)
+    # Run HR-VITON to generate final image
+    print("\nRun HR-VITON to generate final image\n")
+    os.chdir("./HR-VITON-main")
+    terminnal_command = "python3 test_generator.py --cuda True --test_name test1 --tocg_checkpoint mtviton.pth --gpu_ids 0 --gen_checkpoint gen.pth --datasetting unpaired --data_list t2.txt --dataroot ./test" 
+    os.system(terminnal_command)
 
-# Resize b and b_img to these exact dimensions
-b = cv2.resize(b, (exact_width, exact_height))
-b_img = cv2.resize(b_img, (exact_width, exact_height))
+    # Add Background or Not
+    l=glob.glob("./Output/*.png")
 
-# Now, the dimensions of b and b_img should match the slice of img
-# You can proceed with the assignment without encountering the ValueError
-img[int((1024-exact_height)/2): 1024-int((1024-exact_height)/2), int((768-exact_width)/2):768-int((768-exact_width)/2)] = b
-seg_img[int((1024-exact_height)/2): 1024-int((1024-exact_height)/2), int((768-exact_width)/2):768-int((768-exact_width)/2)] = b_img
+    # Add Background
+    if opt.background:
+        for i in l:
+            img=cv2.imread(i)
+            img=cv2.bitwise_and(img,img,mask=mask_img)
+            img=img+back_ground
+            cv2.imwrite(i,img)
 
-# Save the images
-cv2.imwrite("./HR-VITON-main/test/test/cloth/00001_00.jpg", img)
-cv2.imwrite("./HR-VITON-main/test/test/cloth-mask/00001_00.jpg", seg_img)
+    # Remove Background
+    else:
+        for i in l:
+            img=cv2.imread(i)
+            cv2.imwrite(i,img)
+
+    os.chdir("../")
+    cv2.imwrite("./static/finalimg.png", img)
